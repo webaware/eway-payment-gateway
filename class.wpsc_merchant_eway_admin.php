ea6bdd2b0598d029fbed51f4e9da2046e4cee7bf
Ross McKay <rmckay@webaware.com.au>
2013-01-20 07:56:11 +0000

    tag 2.3.1

---
+++ class.wpsc_merchant_eway_admin.php
@@ -0,0 +1,218 @@
+<?php
+
+/**
+* admin functions for eWAY Payment Gateway
+*/
+class wpsc_merchant_eway_admin {
+
+	/**
+	* hook billing details display on admin, to show eWAY transaction number
+	*/
+	public static function actionBillingDetailsBottom() {
+		global $purchlogitem;
+
+		if (!empty($purchlogitem->extrainfo->transactid))
+			echo '<p><strong>Transaction ID: ', htmlspecialchars($purchlogitem->extrainfo->transactid), "</strong></p>\n";
+	}
+
+	/**
+	* display additional fields for gateway config form
+	*/
+	public static function configForm() {
+		$eway_stored = get_option('wpsc_merchant_eway_stored');
+		$eway_stored_yes = $eway_stored ? 'checked="checked"' : '';
+		$eway_stored_no  = $eway_stored ? '' : 'checked="checked"';
+
+		$eway_test = get_option('eway_test');
+		$eway_test_yes = $eway_test ? 'checked="checked"' : '';
+		$eway_test_no  = $eway_test ? '' : 'checked="checked"';
+
+		$eway_th = get_option('wpsc_merchant_eway_th');
+		$eway_th_yes = $eway_th ? 'checked="checked"' : '';
+		$eway_th_no  = $eway_th ? '' : 'checked="checked"';
+
+		$ewayCustomerID = get_option('ewayCustomerID_id');
+		$yes = TXT_WPSC_YES;
+		$no = TXT_WPSC_NO;
+
+		$eway_form_first_name_fields = nzshpcrt_form_field_list(get_option('eway_form_first_name'));
+		$eway_form_last_name_fields = nzshpcrt_form_field_list(get_option('eway_form_last_name'));
+		$eway_form_address_fields = nzshpcrt_form_field_list(get_option('eway_form_address'));
+		$eway_form_city_fields = nzshpcrt_form_field_list(get_option('eway_form_city'));
+		$eway_form_state_fields = nzshpcrt_form_field_list(get_option('eway_form_state'));
+		$eway_form_post_code_fields = nzshpcrt_form_field_list(get_option('eway_form_post_code'));
+		$eway_form_country_fields = nzshpcrt_form_field_list(get_option('eway_form_country'));
+		$eway_form_email_fields = nzshpcrt_form_field_list(get_option('eway_form_email'));
+
+		return <<<EOT
+	<tr>
+		<td>eWAY Customer ID</td>
+		<td>
+			<input type='text' size='10' value="$ewayCustomerID" name='ewayCustomerID_id' />
+		</td>
+	</tr>
+	<tr>
+		<td>Use Stored Payments</td>
+		<td>
+			<label><input type='radio' value='1' name='eway_stored' $eway_stored_yes /> $yes</label> &nbsp;
+			<label><input type='radio' value='0' name='eway_stored' $eway_stored_no /> $no</label>
+		</td>
+	</tr>
+	<tr id="wpsc-eway-admin-stored-test">
+		<td colspan='2' style='color:#c00'>
+			Stored Payments use the Direct Payments sandbox;
+			<br />there is no Stored Payments sandbox.
+		</td>
+	</tr>
+	<tr>
+		<td>Use Testing Enviroment</td>
+		<td>
+			<label><input type='radio' value='1' name='eway_test' $eway_test_yes /> $yes</label> &nbsp;
+			<label><input type='radio' value='0' name='eway_test' $eway_test_no /> $no</label>
+		</td>
+	</tr>
+	<tr>
+		<td>Use TH for field labels</td>
+		<td>
+			<label><input type='radio' value='1' name='eway_th' $eway_th_yes /> $yes</label> &nbsp;
+			<label><input type='radio' value='0' name='eway_th' $eway_th_no /> $no</label>
+		</td>
+	</tr>
+
+	<tr>
+		<td>First Name</td>
+		<td>
+			<select name='eway_form[first_name]'>
+				$eway_form_first_name_fields
+			</select>
+		</td>
+	</tr>
+	<tr>
+		<td>Last Name</td>
+		<td>
+			<select name='eway_form[last_name]'>
+				$eway_form_last_name_fields
+			</select>
+		</td>
+	</tr>
+	<tr>
+		<td>Address Field</td>
+		<td>
+			<select name='eway_form[address]'>
+				$eway_form_address_fields
+			</select>
+		</td>
+	</tr>
+	<tr>
+		<td>City Field</td>
+		<td>
+			<select name='eway_form[city]'>
+				$eway_form_city_fields
+			</select>
+		</td>
+	</tr>
+	<tr>
+		<td>State Field</td>
+		<td>
+			<select name='eway_form[state]'>
+				$eway_form_state_fields
+			</select>
+		</td>
+	</tr>
+	<tr>
+		<td>Postal code/Zip code Field</td>
+		<td>
+			<select name='eway_form[post_code]'>
+				$eway_form_post_code_fields
+			</select>
+		</td>
+	</tr>
+	<tr>
+		<td>Country Field</td>
+		<td>
+			<select name='eway_form[country]'>
+				$eway_form_country_fields
+			</select>
+		</td>
+	</tr>
+	<tr>
+		<td>Email Field</td>
+		<td>
+			<select name='eway_form[email]'>
+				$eway_form_email_fields
+			</select>
+		</td>
+	</tr>
+
+<script>
+//<![CDATA[
+jQuery(function($) {
+
+	/**
+	* check whether both the sandbox (test) mode and Stored Payments are selected,
+	* show warning message if they are
+	*/
+	function checkStoredSandbox() {
+		var	useTest = ($("input[name='eway_test']:checked").val() == "1"),
+			useStored = ($("input[name='eway_stored']:checked").val() == "1");
+
+		if (useTest && useStored) {
+			$("#wpsc-eway-admin-stored-test").show(750);
+		}
+		else {
+			$("#wpsc-eway-admin-stored-test").hide();
+		}
+	}
+
+	$("input[name='eway_test'],input[name='eway_stored']").change(checkStoredSandbox);
+
+	checkStoredSandbox();
+
+});
+//]]>
+</script>
+
+EOT;
+	}
+
+	/**
+	* save config details from payment gateway admin
+	*/
+	public static function saveConfig() {
+		if (isset($_POST['ewayCustomerID_id'])) {
+			update_option('ewayCustomerID_id', $_POST['ewayCustomerID_id']);
+		}
+
+		if (isset($_POST['eway_stored'])) {
+			update_option('wpsc_merchant_eway_stored', $_POST['eway_stored']);
+		}
+
+		if (isset($_POST['eway_test'])) {
+			update_option('eway_test', $_POST['eway_test']);
+		}
+
+		if (isset($_POST['eway_th'])) {
+			update_option('wpsc_merchant_eway_th', $_POST['eway_th']);
+		}
+
+		foreach ((array)$_POST['eway_form'] as $form => $value) {
+			update_option(('eway_form_'.$form), $value);
+		}
+
+		return true;
+	}
+
+	/**
+	* action hook for adding plugin details links
+	*/
+	public static function addPluginDetailsLinks($links, $file) {
+		if ($file == WPSC_MERCH_EWAY_PLUGIN_NAME) {
+			$links[] = '<a href="http://wordpress.org/support/plugin/eway-payment-gateway">' . __('Get help') . '</a>';
+			$links[] = '<a href="http://wordpress.org/extend/plugins/eway-payment-gateway/">' . __('Rating') . '</a>';
+			$links[] = '<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=CXNFEP4EAMTG6">' . __('Donate') . '</a>';
+		}
+
+		return $links;
+	}
+
+}
---
+++ wpsc_merchant_eway.php
@@ -0,0 +1,107 @@
+<?php
+/*
+Plugin Name: eWAY Payment Gateway
+Plugin URI: http://snippets.webaware.com.au/wordpress-plugins/eway-payment-gateway/
+Description: eWAY payment gateway for wp-e-commerce
+Version: 2.3.1
+Author: WebAware
+Author URI: http://www.webaware.com.au/
+*/
+
+/*
+copyright (c) 2011-2013 WebAware Pty Ltd (email : rmckay@webaware.com.au)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+if (!defined('WPSC_MERCH_EWAY_PLUGIN_ROOT')) {
+	define('WPSC_MERCH_EWAY_PLUGIN_ROOT', dirname(__FILE__) . '/');
+	define('WPSC_MERCH_EWAY_PLUGIN_NAME', basename(dirname(__FILE__)) . '/' . basename(__FILE__));
+
+	// wp-e-commerce gateway name
+	define('WPSC_MERCH_EWAY_GATEWAY_NAME', 'wpsc_merchant_eway');
+
+	// name used as cURL user agent
+	define('WPSC_MERCH_EWAY_CURL_USER_AGENT', 'wp-e-commerce eWAY Payment Gateway');
+}
+
+/**
+* autoload classes as/when needed
+* @param string $class_name name of class to attempt to load
+*/
+function wpsc_merchant_eway_autoload($class_name) {
+	static $classMapPlugin = array (
+		// application classes
+		'wpsc_merchant_eway'				=> 'class.wpsc_merchant_eway.php',
+		'wpsc_merchant_eway_admin'			=> 'class.wpsc_merchant_eway_admin.php',
+		'wpsc_merchant_eway_payment'		=> 'class.wpsc_merchant_eway_payment.php',
+		'wpsc_merchant_eway_stored_payment'	=> 'class.wpsc_merchant_eway_stored_payment.php',
+	);
+
+	if (isset($classMapPlugin[$class_name])) {
+		require WPSC_MERCH_EWAY_PLUGIN_ROOT . $classMapPlugin[$class_name];
+	}
+}
+spl_autoload_register('wpsc_merchant_eway_autoload');
+
+// hook wp-e-commerce to extend purchase logs display
+add_action('wpsc_billing_details_bottom', array('wpsc_merchant_eway_admin', 'actionBillingDetailsBottom'));
+
+// hook for adding links to plugin info
+add_filter('plugin_row_meta', array('wpsc_merchant_eway_admin', 'addPluginDetailsLinks'), 10, 2);
+
+/**
+* filter for registering new wp-e-commerce payment gateways
+* @param array $gateways array of registered gateways
+* @return array
+*/
+function wpsc_merchant_eway_register($gateways) {
+	global $gateway_checkout_form_fields;
+
+	// register the gateway class and additional functions
+	$gateways[] = array(
+			'name' => 'eWAY payment gateway',
+			'api_version' => 2.0,
+			'internalname' => WPSC_MERCH_EWAY_GATEWAY_NAME,
+			'class_name' => 'wpsc_merchant_eway',
+			'has_recurring_billing' => FALSE,
+			'wp_admin_cannot_cancel' => FALSE,
+			'display_name' => 'eWAY Credit Card Payment',
+			'form' => 'wpsc_merchant_eway_admin_configForm',	// must call proxy to admin method due to wp-e-commerce shortcomings
+			'submit_function' => array('wpsc_merchant_eway_admin', 'saveConfig'),
+			'payment_type' => 'credit_card',
+			'requirements' => array(
+				'php_version' => 5.0,
+			),
+		);
+
+	// register extra fields we require on the checkout form
+	wpsc_merchant_eway::setCheckoutFields();
+
+	return $gateways;
+}
+add_filter('wpsc_gateway_modules', 'wpsc_merchant_eway_register');
+
+/**
+* proxy to admin method due to wp-e-commerce shortcomings
+* TODO: get wp-e-commerce to use call_user_func() for this call, then can remove proxy function
+*/
+function wpsc_merchant_eway_admin_configForm() {
+	return wpsc_merchant_eway_admin::configForm();
+}
+
+/**
+* custom exceptons
+*/
+class wpsc_merchant_eway_exception extends Exception {}
