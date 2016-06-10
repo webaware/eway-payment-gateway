6e75695afb9a415a620fd845061a2b69130761f0
Ross McKay <rmckay@webaware.com.au>
2016-06-11 09:26:01 +1000

    admin settings forms accessibility improvements

--- views/admin-events-manager.php
+++ views/admin-events-manager.php
@@ -6,19 +6,27 @@ if (!defined('ABSPATH')) {
 }
 ?>
 
+<style>
+.form-table td em {
+	clear:both;
+	display:block;
+	margin:0.4em;
+}
+</style>
+
 <table class="form-table">
 <tbody>
   <tr valign="top">
-	  <th scope="row"><?php _e('Success Message', 'em-pro') ?></th>
+	  <th scope="row"><label for="em_eway_booking_feedback"><?php _e('Success Message', 'em-pro') ?></label></th>
 	  <td>
-		<input type="text" name="em_eway_booking_feedback" value="<?php echo esc_attr(get_option('em_eway_booking_feedback')); ?>" class="large-text" /><br />
+		<input type="text" name="em_eway_booking_feedback" id="em_eway_booking_feedback" value="<?php echo esc_attr(get_option('em_eway_booking_feedback')); ?>" class="large-text" />
 		<em><?php _e('The message that is shown to a user when a booking is successful and payment has been taken.','em-pro'); ?></em>
 	  </td>
   </tr>
   <tr valign="top">
-	  <th scope="row"><?php _e('Success Free Message', 'em-pro') ?></th>
+	  <th scope="row"><label for="em_eway_booking_feedback_free"><?php _e('Success Free Message', 'em-pro') ?></label></th>
 	  <td>
-		<input type="text" name="em_eway_booking_feedback_free" value="<?php echo esc_attr(get_option('em_eway_booking_feedback_free')); ?>" class="large-text" /><br />
+		<input type="text" name="em_eway_booking_feedback_free" id="em_eway_booking_feedback_free" value="<?php echo esc_attr(get_option('em_eway_booking_feedback_free')); ?>" class="large-text" />
 		<em><?php _e('If some cases if you allow a free ticket (e.g. pay at gate) as well as paid tickets, this message will be shown and the user will not be charged.','em-pro'); ?></em>
 	  </td>
   </tr>
@@ -31,9 +39,9 @@ if (!defined('ABSPATH')) {
 <tbody>
 
 	<tr valign="top">
-		<th scope="row"><?php _e('Mode', 'em-pro'); ?></th>
+		<th scope="row"><label for="em_eway_mode"><?php _e('Mode', 'em-pro'); ?></label></th>
 		<td>
-			<select name="em_eway_mode">
+			<select name="em_eway_mode" id="em_eway_mode">
 				<?php $selected = get_option('em_eway_mode'); ?>
 				<option value="sandbox" <?php selected($selected, 'sandbox'); ?>><?php _e('Sandbox','emp-pro'); ?></option>
 				<option value="live" <?php selected($selected, 'live'); ?>><?php _e('Live','emp-pro'); ?></option>
@@ -42,22 +50,22 @@ if (!defined('ABSPATH')) {
 	</tr>
 
 	<tr valign="top">
-		<th scope="row">eWAY customer ID</th>
-		<td><input type="text" name="em_eway_cust_id" value="<?php echo esc_attr(get_option('em_eway_cust_id')); ?>" /></td>
+		<th scope="row"><label for="em_eway_cust_id">eWAY customer ID</label></th>
+		<td><input type="text" name="em_eway_cust_id" id="em_eway_cust_id" value="<?php echo esc_attr(get_option('em_eway_cust_id')); ?>" /></td>
 	</tr>
 
 	<tr valign="top">
-		<th scope="row">Credit card message</th>
+		<th scope="row"><label for="em_eway_card_msg">Credit card message</label></th>
 		<td>
-			<input type="text" name="em_eway_card_msg" value="<?php echo esc_attr(get_option('em_eway_card_msg')); ?>" class="large-text" />
-			<em><br />Message to show above credit card fields, e.g. &quot;Visa and Mastercard only&quot;</em>
+			<input type="text" name="em_eway_card_msg" id="em_eway_card_msg" value="<?php echo esc_attr(get_option('em_eway_card_msg')); ?>" class="large-text" />
+			<em>Message to show above credit card fields, e.g. &quot;Visa and Mastercard only&quot;</em>
 		</td>
 	</tr>
 
 	<tr valign="top">
-		<th scope="row">Force ID 87654321 for sandbox</th>
+		<th scope="row"><label for="em_eway_test_force">Force ID 87654321 for sandbox</label></th>
 		<td>
-			<select name="em_eway_test_force">
+			<select name="em_eway_test_force" id="em_eway_test_force">
 				<?php $selected = get_option('em_eway_test_force'); ?>
 				<option value="1" <?php selected($selected, '1'); ?>>Yes</option>
 				<option value="0" <?php selected($selected, '0'); ?>>No</option>
@@ -66,9 +74,9 @@ if (!defined('ABSPATH')) {
 	</tr>
 
 	<tr valign="top">
-		<th scope="row">Force SSL for bookings form</th>
+		<th scope="row"><label for="em_eway_ssl_force">Force SSL for bookings form</label></th>
 		<td>
-			<select name="em_eway_ssl_force">
+			<select name="em_eway_ssl_force" id="em_eway_ssl_force">
 				<?php $selected = get_option('em_eway_ssl_force'); ?>
 				<option value="1" <?php selected($selected, '1'); ?>>Yes</option>
 				<option value="0" <?php selected($selected, '0'); ?>>No</option>
@@ -77,57 +85,57 @@ if (!defined('ABSPATH')) {
 	</tr>
 
 	<tr valign="top">
-		<th scope="row">Logging</th>
+		<th scope="row"><label for="em_eway_logging">Logging</label></th>
 		<td>
-			<select name="em_eway_logging">
+			<select name="em_eway_logging" id="em_eway_logging">
 				<?php $selected = get_option('em_eway_logging'); ?>
 				<option value="off" <?php selected($selected, 'off'); ?>>Off</option>
 				<option value="info" <?php selected($selected, 'info'); ?>>All messages</option>
 				<option value="error" <?php selected($selected, 'error'); ?>>Errors only</option>
-			</select><br />
-			<em>Enable logging to assist trouble shooting. The log file can be found in:</em><br />
+			</select>
+			<em>Enable logging to assist trouble shooting. The log file can be found in:</em>
 			<em><?php echo esc_html(substr(EwayPaymentsLogging::getLogFolder(), strlen(ABSPATH))); ?></em>
 		</td>
 	</tr>
 
 	<tr valign="top">
-		<th scope="row">Stored payments</th>
+		<th scope="row"><label for="em_eway_stored">Stored payments</label></th>
 		<td>
-			<select name="em_eway_stored">
+			<select name="em_eway_stored" id="em_eway_stored">
 				<?php $selected = get_option('em_eway_stored'); ?>
 				<option value="1" <?php selected($selected, '1'); ?>>Yes</option>
 				<option value="0" <?php selected($selected, '0'); ?>>No</option>
-			</select><br />
+			</select>
 			<em>Stored payments records payment details but doesn't bill immediately.</em>
-			<em id="em-eway-admin-stored-test" style='color:#c00'><br />Stored Payments uses the Direct Payments sandbox; there is no Stored Payments sandbox.</em>
+			<em id="em-eway-admin-stored-test" style='color:#c00'>Stored Payments uses the Direct Payments sandbox; there is no Stored Payments sandbox.</em>
 		</td>
 	</tr>
 
 	<tr valign="top">
-		<th scope="row">Beagle (free)</th>
+		<th scope="row"><label for="em_eway_beagle">Beagle (free)</label></th>
 		<td>
-			<select name="em_eway_beagle">
+			<select name="em_eway_beagle" id="em_eway_beagle">
 				<?php $selected = get_option('em_eway_beagle'); ?>
 				<option value="1" <?php selected($selected, '1'); ?>>Yes</option>
 				<option value="0" <?php selected($selected, '0'); ?>>No</option>
-			</select><br />
+			</select>
 			<em><a href="https://www.eway.com.au/developers/api/beagle-lite" target="_blank">Beagle</a>
 			 is a service from eWAY that provides a level of fraud protection for your transactions.
 			 It uses information about the IP address of the purchaser to suggest whether there is a risk of fraud.
 			 You must configure Beagle rules in your MYeWAY console before enabling Beagle.</em>
-			<em><br />You will also need to add a Country field to your booking form. Beagle works by comparing the country of the address with
+			<em>You will also need to add a Country field to your booking form. Beagle works by comparing the country of the address with
 			the country where the purchaser is using the Internet; Beagle won't be used when booking without a country selected.</em>
-			<em id="em-eway-admin-stored-beagle" style='color:#c00'><br />Beagle is not available for Stored Payments</em>
+			<em id="em-eway-admin-stored-beagle" style='color:#c00'>Beagle is not available for Stored Payments</em>
 		</td>
 	</tr>
 
-	<tr><td colspan="2"><strong><?php echo sprintf(__( '%s Options', 'dbem' ),__('Advanced','em-pro')); ?></strong></td></tr>
+	<tr><th colspan="2"><?php echo sprintf(__( '%s Options', 'dbem' ),__('Advanced','em-pro')); ?></th></tr>
 
 	<tr valign="top">
-	  <th scope="row"><?php _e('Manually approve completed transactions?', 'em-pro') ?></th>
+	  <th scope="row"><label for="em_eway_manual_approval"><?php _e('Manually approve completed transactions?', 'em-pro') ?></label></th>
 	  <td>
-		<input type="checkbox" name="em_eway_manual_approval" value="1" <?php echo (get_option('em_eway_manual_approval')) ? 'checked="checked"':''; ?> /><br />
-		<em><?php _e('By default, when someone pays for a booking, it gets automatically approved once the payment is confirmed. If you would like to manually verify and approve bookings, tick this box.','em-pro'); ?></em><br />
+		<input type="checkbox" name="em_eway_manual_approval" id="em_eway_manual_approval" value="1" <?php echo (get_option('em_eway_manual_approval')) ? 'checked="checked"':''; ?> />
+		<em><?php _e('By default, when someone pays for a booking, it gets automatically approved once the payment is confirmed. If you would like to manually verify and approve bookings, tick this box.','em-pro'); ?></em>
 		<em><?php echo sprintf(__('Approvals must also be required for all bookings in your <a href="%s">settings</a> for this to work properly.','em-pro'),EM_ADMIN_URL.'&amp;page=events-manager-options'); ?></em>
 	  </td>
 	</tr>
--- views/admin-wpsc.php
+++ views/admin-wpsc.php
@@ -13,145 +13,156 @@ $eway_logging	= get_option('eway_logging', 'off');
 
 ?>
 
-	<tr>
-		<td>eWAY Customer ID</td>
+	<tr valign="top">
+		<th scope="row"><label for="ewayCustomerID_id">eWAY Customer ID</label></th>
 		<td>
-			<input type='text' size='10' value="<?php echo esc_attr(get_option('ewayCustomerID_id')); ?>" name='ewayCustomerID_id' />
+			<input type="text" size="10" value="<?php echo esc_attr(get_option('ewayCustomerID_id')); ?>" name="ewayCustomerID_id" id="ewayCustomerID_id" />
 		</td>
 	</tr>
 
-	<tr>
-		<td>Use Stored Payments</td>
+	<tr valign="top">
+		<th scope="row" id="eway_stored_label">Use Stored Payments</th>
 		<td>
-			<label><input type='radio' value='1' name='eway_stored' <?php checked($eway_stored, '1'); ?> /> <?php echo TXT_WPSC_YES; ?></label> &nbsp;
-			<label><input type='radio' value='0' name='eway_stored' <?php checked($eway_stored, '0'); ?> /> <?php echo TXT_WPSC_NO; ?></label>
+			<input type="radio" value="1" name="eway_stored" id="eway_stored_1" <?php checked($eway_stored, '1'); ?> aria-labelledby="eway_stored_label eway_stored_label_1" />
+			<label for="eway_stored_1" id="eway_stored_label_1"><?php echo TXT_WPSC_YES; ?></label> &nbsp;
+			<input type="radio" value="0" name="eway_stored" id="eway_stored_0" <?php checked($eway_stored, '0'); ?> aria-labelledby="eway_stored_label eway_stored_label_0" />
+			<label for="eway_stored_0" id="eway_stored_label_0"><?php echo TXT_WPSC_NO; ?></label>
 		</td>
 	</tr>
-	<tr id="wpsc-eway-admin-stored-test">
-		<td colspan='2' style='color:#c00'>
+	<tr id="wpsc-eway-admin-stored-test" valign="top">
+		<td colspan="2" style="color:#c00">
 			Stored Payments uses the Direct Payments sandbox;
 			<br />there is no Stored Payments sandbox.
 		</td>
 	</tr>
 
-	<tr>
-		<td>Use Testing Enviroment</td>
+	<tr valign="top">
+		<th scope="row" id="eway_test_label">Use Testing Enviroment</th>
 		<td>
-			<label><input type='radio' value='1' name='eway_test' <?php checked($eway_test, '1'); ?> /> <?php echo TXT_WPSC_YES; ?></label> &nbsp;
-			<label><input type='radio' value='0' name='eway_test' <?php checked($eway_test, '0'); ?> /> <?php echo TXT_WPSC_NO; ?></label>
+			<input type="radio" value="1" name="eway_test" id="eway_test_1" <?php checked($eway_test, '1'); ?> aria-labelledby="eway_test_label eway_test_label_1" />
+			<label for="eway_test_1" id="eway_test_label_1"><?php echo TXT_WPSC_YES; ?></label> &nbsp;
+			<input type="radio" value="0" name="eway_test" id="eway_test_0" <?php checked($eway_test, '0'); ?> aria-labelledby="eway_test_label eway_test_label_0" />
+			<label for="eway_test_0" id="eway_test_label_0"><?php echo TXT_WPSC_NO; ?></label>
 		</td>
 	</tr>
 
-	<tr>
-		<td>Logging</td>
+	<tr valign="top">
+		<th scope="row" id="eway_logging_label">Logging</th>
 		<td>
-			<label><input type='radio' value='off' name='eway_logging' <?php checked($eway_logging, 'off'); ?> /> Off</label> &nbsp;
-			<label><input type='radio' value='info' name='eway_logging' <?php checked($eway_logging, 'info'); ?> /> All messages</label>
-			<label><input type='radio' value='error' name='eway_logging' <?php checked($eway_logging, 'error'); ?> /> Errors only</label> &nbsp;
+			<input type="radio" value="off" name="eway_logging" id="eway_logging_off" <?php checked($eway_logging, 'off'); ?> aria-labelledby="eway_logging_label eway_logging_label_off" />
+			<label for="eway_logging_off" id="eway_logging_label_off">Off</label> &nbsp;
+			<input type="radio" value="info" name="eway_logging" id="eway_logging_info" <?php checked($eway_logging, 'info'); ?> aria-labelledby="eway_logging_label eway_logging_label_info" />
+			<label for="eway_logging_info" id="eway_logging_label_info">All messages</label> &nbsp;
+			<input type="radio" value="error" name="eway_logging" id="eway_logging_error" <?php checked($eway_logging, 'error'); ?> aria-labelledby="eway_logging_label eway_logging_label_error" />
+			<label for="eway_logging_error" id="eway_logging_label_error">Errors only</label> &nbsp;
 			<p class="description">enable logging to assist trouble shooting;
 			<br />the log file can be found in <?php echo esc_html(substr(EwayPaymentsLogging::getLogFolder(), strlen(ABSPATH))); ?></p>
 		</td>
 	</tr>
 
-	<tr>
-		<td>Use TH for field labels</td>
+	<tr valign="top">
+		<th scope="row" id="eway_th_label">Use TH for field labels</th>
 		<td>
-			<label><input type='radio' value='1' name='eway_th' <?php checked($eway_th, '1'); ?> /> <?php echo TXT_WPSC_YES; ?></label> &nbsp;
-			<label><input type='radio' value='0' name='eway_th' <?php checked($eway_th, '0'); ?> /> <?php echo TXT_WPSC_NO; ?></label>
+			<input type="radio" value="1" name="eway_th" id="eway_th_1" <?php checked($eway_th, '1'); ?> aria-labelledby="eway_th_label eway_th_label_1" />
+			<label for="eway_th_1" id="eway_th_label_1"><?php echo TXT_WPSC_YES; ?></label> &nbsp;
+			<input type="radio" value="0" name="eway_th" id="eway_th_0" <?php checked($eway_th, '0'); ?> aria-labelledby="eway_th_label eway_th_label_0" />
+			<label for="eway_th_0" id="eway_th_label_0"><?php echo TXT_WPSC_NO; ?></label>
 		</td>
 	</tr>
 
-	<tr>
-		<td>Use <a href="https://www.eway.com.au/developers/api/beagle-lite" target="_blank">Beagle</a></td>
+	<tr valign="top">
+		<th scope="row" id="eway_beagle_label">Use <a href="https://www.eway.com.au/developers/api/beagle-lite" target="_blank">Beagle</a></th>
 		<td>
-			<label><input type='radio' value='1' name='eway_beagle' <?php checked($eway_beagle, '1'); ?> /> <?php echo TXT_WPSC_YES; ?></label> &nbsp;
-			<label><input type='radio' value='0' name='eway_beagle' <?php checked($eway_beagle, '0'); ?> /> <?php echo TXT_WPSC_NO; ?></label>
+			<input type="radio" value="1" name="eway_beagle" id="eway_beagle_1" <?php checked($eway_beagle, '1'); ?> aria-labelledby="eway_beagle_label eway_beagle_label_1" />
+			<label for="eway_beagle_1" id="eway_beagle_label_1"><?php echo TXT_WPSC_YES; ?></label> &nbsp;
+			<input type="radio" value="0" name="eway_beagle" id="eway_beagle_0" <?php checked($eway_beagle, '0'); ?> aria-labelledby="eway_beagle_label eway_beagle_label_0" />
+			<label for="eway_beagle_0" id="eway_beagle_label_0"><?php echo TXT_WPSC_NO; ?></label>
 			<span id="wpsc-eway-admin-beagle-address">
 				<br />You will also need to add a Country field to your checkout form. Beagle works by comparing the country of the address with the
 				country where the purchaser is using the Internet; Beagle won't be used when checking out without a country selected.
 			</span>
 		</td>
 	</tr>
-	<tr id="wpsc-eway-admin-stored-beagle">
-		<td colspan='2' style='color:#c00'>
+	<tr id="wpsc-eway-admin-stored-beagle" valign="top">
+		<td colspan="2" style="color:#c00">
 			Beagle is not available for Stored Payments
 		</td>
 	</tr>
 
-	<tr>
-		<td>Credit card message</td>
+	<tr valign="top">
+		<th scope="row"><label for="eway_card_msg">Credit card message</label></th>
 		<td>
-			<input type="text" style="width:100%" value="<?php echo esc_attr(get_option('wpsc_merchant_eway_card_msg')); ?>" name="eway_card_msg" />
+			<input type="text" style="width:100%" value="<?php echo esc_attr(get_option('wpsc_merchant_eway_card_msg')); ?>" name="eway_card_msg" id="eway_card_msg" />
 		</td>
 	</tr>
 
-	<tr>
-		<td>First Name</td>
+	<tr valign="top">
+		<th scope="row"><label for="eway_form_first_name">First Name</label></th>
 		<td>
-			<select name='eway_form[first_name]'>
+			<select name="eway_form[first_name]" id="eway_form_first_name">
 				<?php EwayPaymentsWpsc::showCheckoutFormFields(get_option('eway_form_first_name')); ?>
 			</select>
 		</td>
 	</tr>
 
-	<tr>
-		<td>Last Name</td>
+	<tr valign="top">
+		<th scope="row"><label for="eway_form_last_name">Last Name</label></th>
 		<td>
-			<select name='eway_form[last_name]'>
+			<select name="eway_form[last_name]" id="eway_form_last_name">
 				<?php echo EwayPaymentsWpsc::showCheckoutFormFields(get_option('eway_form_last_name')); ?>
 			</select>
 		</td>
 	</tr>
 
-	<tr>
-		<td>Address Field</td>
+	<tr valign="top">
+		<th scope="row"><label for="eway_form_address">Address Field</label></th>
 		<td>
-			<select name='eway_form[address]'>
+			<select name="eway_form[address]" id="eway_form_address">
 				<?php echo EwayPaymentsWpsc::showCheckoutFormFields(get_option('eway_form_address')); ?>
 			</select>
 		</td>
 	</tr>
 
-	<tr>
-		<td>City Field</td>
+	<tr valign="top">
+		<th scope="row"><label for="eway_form_city">City Field</label></th>
 		<td>
-			<select name='eway_form[city]'>
+			<select name="eway_form[city]" id="eway_form_city">
 				<?php echo EwayPaymentsWpsc::showCheckoutFormFields(get_option('eway_form_city')); ?>
 			</select>
 		</td>
 	</tr>
 
-	<tr>
-		<td>State Field</td>
+	<tr valign="top">
+		<th scope="row"><label for="eway_form_state">State Field</label></th>
 		<td>
-			<select name='eway_form[state]'>
+			<select name="eway_form[state]" id="eway_form_state">
 				<?php echo EwayPaymentsWpsc::showCheckoutFormFields(get_option('eway_form_state')); ?>
 			</select>
 		</td>
 	</tr>
 
-	<tr>
-		<td>Postal code/Zip code Field</td>
+	<tr valign="top">
+		<th scope="row"><label for="eway_form_post_code">Postal code/Zip code Field</label></th>
 		<td>
-			<select name='eway_form[post_code]'>
+			<select name="eway_form[post_code]" id="eway_form_post_code">
 				<?php echo EwayPaymentsWpsc::showCheckoutFormFields(get_option('eway_form_post_code')); ?>
 			</select>
 		</td>
 	</tr>
 
-	<tr>
-		<td>Country Field</td>
+	<tr valign="top">
+		<th scope="row"><label for="eway_form_country">Country Field</label></th>
 		<td>
-			<select name='eway_form[country]'>
+			<select name="eway_form[country]" id="eway_form_country">
 				<?php echo EwayPaymentsWpsc::showCheckoutFormFields(get_option('eway_form_country')); ?>
 			</select>
 		</td>
 	</tr>
 
-	<tr>
-		<td>Email Field</td>
+	<tr valign="top">
+		<th scope="row"><label for="eway_form_email">Email Field</label></th>
 		<td>
-			<select name='eway_form[email]'>
+			<select name="eway_form[email]" id="eway_form_email">
 				<?php echo EwayPaymentsWpsc::showCheckoutFormFields(get_option('eway_form_email')); ?>
 			</select>
 		</td>
