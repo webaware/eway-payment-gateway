b0a32da91ca196457336e8e1884ac0c10d25b14f
Ross McKay <rmckay@webaware.com.au>
2016-06-10 13:57:37 +1000

    update "requires" messages

--- views/requires-extensions.php
+++ views/requires-extensions.php
@@ -6,9 +6,9 @@ if (!defined('ABSPATH')) {
 
 <div class="error">
 	<p>eWAY Payment Gateway requires these missing PHP extensions. Please contact your website host to have these extensions installed.</p>
-	<ul style="list-style-type: disc; padding-left: 2em;">
-	<?php foreach ($missing as $ext): ?>
-		<li><?php echo esc_html($ext); ?></li>
-	<?php endforeach; ?>
+	<ul style="padding-left: 2em">
+		<?php foreach ($missing as $ext): ?>
+		<li style="list-style-type:disc"><?php echo esc_html($ext); ?></li>
+		<?php endforeach; ?>
 	</ul>
 </div>
