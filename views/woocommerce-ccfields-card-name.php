5f4bfaa96a9b4d327709c1a69baae393446336b8
Ross McKay <rmckay@webaware.com.au>
2017-02-04 15:54:15 +1100

    Decorate WooCommerce card holder field with auto* cancel attributes

--- views/woocommerce-ccfields-card-name.php
+++ views/woocommerce-ccfields-card-name.php
@@ -6,5 +6,6 @@ if (!defined('ABSPATH')) {
 
 <p class="form-row form-row-wide">
 	<label for="<?php echo esc_attr($this->id); ?>-card-name"><?php esc_html_e("Card Holder's Name", 'eway-payment-gateway'); ?> <span class="required">*</span></label>
-	<input id="<?php echo esc_attr($this->id); ?>-card-name" class="input-text wc-credit-card-form-card-name" type="text" maxlength="50" autocomplete="off" name="<?php echo esc_attr($this->id); ?>-card-name" />
+	<input id="<?php echo esc_attr($this->id); ?>-card-name" class="input-text wc-credit-card-form-card-name" type="text" maxlength="50" name="<?php echo esc_attr($this->id); ?>-card-name"
+		autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" />
 </p>
