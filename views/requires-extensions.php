3de4ff0eef924adc40217160f23e685dd6ffa891
Ross McKay <rmckay@webaware.com.au>
2018-09-26 17:50:00 +1000

    update error notice classes

--- views/requires-extensions.php
+++ views/requires-extensions.php
@@ -4,7 +4,7 @@ if (!defined('ABSPATH')) {
 }
 ?>
 
-<div class="error">
+<div class="notice notice-error">
 	<p><?php esc_html_e('eWAY Payment Gateway requires these missing PHP extensions. Please contact your website host to have these extensions installed.', 'eway-payment-gateway'); ?></p>
 	<ul style="padding-left: 2em">
 		<?php foreach ($missing as $ext): ?>
--- views/requires-php.php
+++ views/requires-php.php
@@ -4,7 +4,7 @@ if (!defined('ABSPATH')) {
 }
 ?>
 
-<div class="error">
+<div class="notice notice-error">
 	<p><?php printf(__('eWAY Payment Gateway requires PHP %1$s or higher; your website has PHP %2$s which is <a rel="noopener" target="_blank" href="%3$s">old, obsolete, and unsupported</a>.', 'eway-payment-gateway'),
 			esc_html(EWAY_PAYMENTS_MIN_PHP), esc_html(PHP_VERSION), 'https://secure.php.net/supported-versions.php'); ?></p>
 	<p><?php printf(__('Please upgrade your website hosting. At least PHP %s is recommended.', 'eway-payment-gateway'), '7.1'); ?></p>
