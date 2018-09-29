f16465aef8f4c9179515e8b77e1068e97a3b7b96
Ross McKay <rmckay@webaware.com.au>
2018-09-29 11:20:25 +1000

    requires-php.php must be PHP5.3 compatible, also escape strings

--- views/requires-php.php
+++ views/requires-php.php
@@ -6,12 +6,12 @@ if (!defined('ABSPATH')) {
 
 <div class="notice notice-error">
 	<p>
-		<?= eway_payment_gateway_external_link(
-				sprintf(__('eWAY Payment Gateway requires PHP %1$s or higher; your website has PHP %2$s which is {{a}}old, obsolete, and unsupported{{/a}}.', 'eway-payment-gateway'),
+		<?php echo eway_payment_gateway_external_link(
+				sprintf(esc_html__('eWAY Payment Gateway requires PHP %1$s or higher; your website has PHP %2$s which is {{a}}old, obsolete, and unsupported{{/a}}.', 'eway-payment-gateway'),
 					esc_html(EWAY_PAYMENTS_MIN_PHP), esc_html(PHP_VERSION)),
 				'https://secure.php.net/supported-versions.php'
 			);
 		?>
 	</p>
-	<p><?php printf(__('Please upgrade your website hosting. At least PHP %s is recommended.', 'eway-payment-gateway'), '7.1'); ?></p>
+	<p><?php printf(esc_html__('Please upgrade your website hosting. At least PHP %s is recommended.', 'eway-payment-gateway'), '7.1'); ?></p>
 </div>
