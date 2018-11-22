607a2bd4425843d74f0f620a1122b23105af998c
Ross McKay <rmckay@webaware.com.au>
2018-11-22 20:31:46 +1100

    exclude all PHPCS config files from .zip build

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,7 @@ module.exports = function (grunt) {
 							"!./composer.*",
 							"!./Gruntfile.js",
 							"!./package*.json",
-							"!./phpcs.xml",
+							"!./phpcs*.xml",
 						],
 						dest: "dist/<%= pkg.name %>/"
 					}
