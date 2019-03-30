da73d71519f0135ce53da522815c62383aba7838
Ross McKay <rmckay@webaware.com.au>
2019-03-31 10:02:55 +1100

    bump version to 4.2.3-dev, update grunt / composer configs

--- eway-payment-gateway.php
+++ eway-payment-gateway.php
@@ -3,7 +3,7 @@
 Plugin Name: eWAY Payment Gateway
 Plugin URI: https://shop.webaware.com.au/downloads/eway-payment-gateway/
 Description: Integrate some popular WordPress plugins with the eWAY credit card payment gateway
-Version: 4.3.1
+Version: 4.3.2-dev
 Author: WebAware
 Author URI: https://shop.webaware.com.au/
 Text Domain: eway-payment-gateway
@@ -12,7 +12,7 @@ WC tested up to: 3.5
 */
 
 /*
-copyright (c) 2011-2018 WebAware Pty Ltd (email : support@webaware.com.au)
+copyright (c) 2011-2019 WebAware Pty Ltd (email : support@webaware.com.au)
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -37,7 +37,7 @@ define('EWAY_PAYMENTS_PLUGIN_FILE', __FILE__);
 define('EWAY_PAYMENTS_PLUGIN_ROOT', dirname(__FILE__) . '/');
 define('EWAY_PAYMENTS_PLUGIN_NAME', basename(dirname(__FILE__)) . '/' . basename(__FILE__));
 define('EWAY_PAYMENTS_MIN_PHP', '5.6');
-define('EWAY_PAYMENTS_VERSION', '4.3.1');
+define('EWAY_PAYMENTS_VERSION', '4.3.2-dev');
 
 require EWAY_PAYMENTS_PLUGIN_ROOT . 'includes/functions-global.php';
 
